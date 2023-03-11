efd450d91d9416a548a41fc83a86ce468466775d
sartorifbio <sartorifbio@outlook.com>
2023-03-11 20:09:42 -0300

    Add index.html and css/style.css and js/etch-a-scktch.js

---
+++ js/etch-a-scktch.js
@@ -0,0 +1,46 @@
+// variável que recebe a seleção da div html em que será criado o quadro
+const sketchbook = document.querySelector('#sketchbook');
+
+/*define o número de linhas e colunas na grade */
+const grideRowsCols = 16;
+const whiteSquare = 512 / grideRowsCols;
+
+//variável que recebe o tamanho em pixes da altura e largura do quadro
+const whidthHeight = 512;
+
+//definindo altura e largura
+sketchbook.style.width = `${whidthHeight}px`;
+sketchbook.style.height = `${whidthHeight}px`;;
+
+/*criando este estilo porque no style display inline-block que vou criar na iteração abaixo, ficará um espaço entre as divs*/
+sketchbook.style.lineHeight = '0%';
+
+sketchbook.style.margin = '20px';
+
+for (let row=0; row < whiteSquare;row++){
+    for (let col=0;col < whiteSquare;col++){
+        let cell = document.createElement('div');
+
+        cell.style.width = `${grideRowsCols}px`;
+        cell.style.height = `${grideRowsCols}px`;
+        cell.style.backgroundColor = 'white';
+
+        cell.classList.add('cell');
+        cell.style.margin = '0px';
+        cell.style.display = 'inline-block';
+        sketchbook.appendChild(cell);
+
+        function fillCell() {
+            cell.style.backgroundColor = 'black';
+        }
+        
+        sketchbook.addEventListener('mousedown', () => {
+            cell.addEventListener('mousemove', fillCell);
+        });      
+
+        sketchbook.addEventListener('mouseup', () => {
+            cell.removeEventListener('mousemove', fillCell);
+        });
+          
+    };
+};
